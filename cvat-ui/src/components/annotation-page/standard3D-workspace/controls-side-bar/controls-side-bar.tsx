3c611067908ca3b7c6011328de374c51c92fab1f
Will Cheng Yong <chengyong@pku.edu.cn>
2022-07-12 17:50:07 +0800

    Modify controls side bar & add polygon shape.

--- cvat-ui/src/components/annotation-page/standard3D-workspace/controls-side-bar/controls-side-bar.tsx
+++ cvat-ui/src/components/annotation-page/standard3D-workspace/controls-side-bar/controls-side-bar.tsx
@@ -5,13 +5,14 @@
 import React from 'react';
 import Layout from 'antd/lib/layout';
 import { ActiveControl } from 'reducers/interfaces';
-import { Canvas3d as Canvas } from 'cvat-canvas3d-wrapper';
+import { Canvas } from 'cvat-canvas-wrapper';
+import { Canvas3d } from 'cvat-canvas3d-wrapper';
 import MoveControl, {
     Props as MoveControlProps,
-} from 'components/annotation-page/standard-workspace/controls-side-bar/move-control';
+} from './move-control';
 import CursorControl, {
     Props as CursorControlProps,
-} from 'components/annotation-page/standard-workspace/controls-side-bar/cursor-control';
+} from './cursor-control';
 import DrawCuboidControl, {
     Props as DrawCuboidControlProps,
 } from 'components/annotation-page/standard-workspace/controls-side-bar/draw-cuboid-control';
@@ -19,11 +20,15 @@ import GroupControl, {
     Props as GroupControlProps,
 } from 'components/annotation-page/standard-workspace/controls-side-bar/group-control';
 import GlobalHotKeys, { KeyMap } from 'utils/mousetrap-react';
-import ControlVisibilityObserver from 'components/annotation-page/standard-workspace/controls-side-bar/control-visibility-observer';
+import ControlVisibilityObserver from './control-visibility-observer';
+import DrawPolygonControl, {
+    Props as DrawPolygonControlProps,
+} from './draw-polygon-control';
 
 interface Props {
     keyMap: KeyMap;
-    canvasInstance: Canvas;
+    canvasInstance: Canvas3d;
+    canvasInstanceSelection: Canvas;
     activeControl: ActiveControl;
     normalizedKeyMap: Record<string, string>;
     labels: any[];
@@ -38,11 +43,13 @@ interface Props {
 const ObservedCursorControl = ControlVisibilityObserver<CursorControlProps>(CursorControl);
 const ObservedMoveControl = ControlVisibilityObserver<MoveControlProps>(MoveControl);
 const ObservedDrawCuboidControl = ControlVisibilityObserver<DrawCuboidControlProps>(DrawCuboidControl);
+const ObservedDrawPolygonControl = ControlVisibilityObserver<DrawPolygonControlProps>(DrawPolygonControl);
 const ObservedGroupControl = ControlVisibilityObserver<GroupControlProps>(GroupControl);
 
 export default function ControlsSideBarComponent(props: Props): JSX.Element {
     const {
         canvasInstance,
+        canvasInstanceSelection,
         pasteShape,
         activeControl,
         normalizedKeyMap,
@@ -70,6 +77,7 @@ export default function ControlsSideBarComponent(props: Props): JSX.Element {
             preventDefault(event);
             if (activeControl !== ActiveControl.CURSOR) {
                 canvasInstance.cancel();
+                canvasInstanceSelection.cancel();
             }
         },
     };
@@ -132,12 +140,22 @@ export default function ControlsSideBarComponent(props: Props): JSX.Element {
             <ObservedCursorControl
                 cursorShortkey={normalizedKeyMap.CANCEL}
                 canvasInstance={canvasInstance}
+                canvasInstanceSelection={canvasInstanceSelection}
                 activeControl={activeControl}
             />
-            <ObservedMoveControl canvasInstance={canvasInstance} activeControl={activeControl} />
+            <ObservedMoveControl
+                canvasInstance={canvasInstance}
+                canvasInstanceSelection={canvasInstanceSelection}
+                activeControl={activeControl}
+            />
             <ObservedDrawCuboidControl
                 canvasInstance={canvasInstance}
                 isDrawing={activeControl === ActiveControl.DRAW_CUBOID}
+                disabled={!labels.length}
+            />
+            <ObservedDrawPolygonControl
+                canvasInstance={canvasInstanceSelection}
+                isDrawing={activeControl === ActiveControl.DRAW_POLYGON}
                 disabled={!labels.length}
             />
             <ObservedGroupControl
